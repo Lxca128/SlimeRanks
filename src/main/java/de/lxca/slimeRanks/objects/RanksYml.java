e8817d9b27677a6b2d2f49ee0f30b24924baf07a
Lxca <luca@lxca.de>
2025-01-19 19:49:05 +0100

    Split the RanksYml in a Yml parent object

--- src/main/java/de/lxca/slimeRanks/objects/RanksYml.java
+++ src/main/java/de/lxca/slimeRanks/objects/RanksYml.java
@@ -1,75 +1,19 @@
 package de.lxca.slimeRanks.objects;
 
-import de.lxca.slimeRanks.Main;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.jetbrains.annotations.Nullable;
-
-import java.io.File;
-import java.io.IOException;
-
-public class RanksYml {
+public class RanksYml extends Yml{
 
     private static final String filePath = "plugins/SlimeRanks/";
     private static final String fileName = "ranks.yml";
-    private static File configFile;
-    private static YamlConfiguration ymlConfig;
 
     public RanksYml() {
-        configFile = createConfigFileIfNotExists();
-        ymlConfig = loadYmlConfig();
-
-        if (ymlConfig.getKeys(true).isEmpty()) {
+        super(filePath, fileName);
+        if (getYmlConfig().getKeys(true).isEmpty()) {
             setDefaultYmlKeys();
         }
     }
 
-    private @Nullable File createConfigFileIfNotExists() {
-        if (!pathExists() && !new File(filePath).mkdirs()) {
-            Main.getLogger(this.getClass()).warn("Failed to create directory " + filePath + ".");
-            return null;
-        }
-        if (!fileExists()) {
-            try {
-                if (!new File(filePath + fileName).createNewFile()) {
-                    Main.getLogger(this.getClass()).warn("Failed to create file " + filePath + fileName + ".");
-                    return null;
-                }
-            } catch (IOException e) {
-                Main.getLogger(this.getClass()).error("Failed to create file " + filePath + fileName + "!");
-                throw new RuntimeException(e);
-            }
-        }
-
-        return new File(filePath + fileName);
-    }
-
-    public YamlConfiguration loadYmlConfig() {
-        if (!fileExists()) {
-            configFile = createConfigFileIfNotExists();
-        }
-
-        if (configFile == null) {
-            Main.getLogger(this.getClass()).error("Failed to load file " + filePath + fileName + "!");
-            return null;
-        }
-
-        ymlConfig = YamlConfiguration.loadConfiguration(configFile);
-        return ymlConfig;
-    }
-
-    public YamlConfiguration getYmlConfig() {
-        return ymlConfig;
-    }
-
-    public void saveYmlConfig() {
-        try {
-            ymlConfig.save(configFile);
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-    }
-
-    private void setDefaultYmlKeys() {
+    @Override
+    protected void setDefaultYmlKeys() {
         createConfigKey("ConfigVersion", 1);
         createConfigKey("Ranks.Admin.Tab.Active", true);
         createConfigKey("Ranks.Admin.Tab.Format", "<red>Admin</red> <dark_gray>-</dark_gray> <red>{player}</red>");
@@ -89,19 +33,5 @@ public class RanksYml {
         createConfigKey("Ranks.Player.NameTag.Format", "<gray>Player</gray> <dark_gray>-</dark_gray> <gray>{player}</gray>");
         createConfigKey("Ranks.Player.NameTag.HideOnSneak", true);
         saveYmlConfig();
-    }
-
-    private void createConfigKey(String key, Object defaultValue) {
-        if (!ymlConfig.getKeys(true).contains(key)) {
-            ymlConfig.set(key, defaultValue);
-        }
-    }
-
-    private static boolean pathExists() {
-        return new File(filePath).exists();
-    }
-
-    private static boolean fileExists() {
-        return new File(filePath + fileName).exists();
     }
 }
