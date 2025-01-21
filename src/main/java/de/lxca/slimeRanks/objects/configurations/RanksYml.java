6dcce8f1eee72b4dd3a677e0aaf9102654c7550f
Lxca <luca@lxca.de>
2025-01-21 19:52:50 +0100

    Adjust the default tab priority in the Ranks.yml

--- src/main/java/de/lxca/slimeRanks/objects/configurations/RanksYml.java
+++ src/main/java/de/lxca/slimeRanks/objects/configurations/RanksYml.java
@@ -17,7 +17,7 @@ public class RanksYml extends Yml {
         createConfigKey("ConfigVersion", 1);
         createConfigKey("Ranks.Admin.Tab.Active", true);
         createConfigKey("Ranks.Admin.Tab.Format", "<red>Admin</red> <dark_gray>-</dark_gray> <red>{player}</red>");
-        createConfigKey("Ranks.Admin.Tab.Priority", 0);
+        createConfigKey("Ranks.Admin.Tab.Priority", 2);
         createConfigKey("Ranks.Admin.Chat.Active", true);
         createConfigKey("Ranks.Admin.Chat.Format", "<red>Admin</red> <dark_gray>-</dark_gray> <red>{player}</red><dark_gray>:</dark_gray> <gray>{message}</gray>");
         createConfigKey("Ranks.Admin.NameTag.Active", true);
@@ -26,7 +26,7 @@ public class RanksYml extends Yml {
         createConfigKey("Ranks.Admin.Permission", "slimeranks.rank.admin");
         createConfigKey("Ranks.Player.Tab.Active", true);
         createConfigKey("Ranks.Player.Tab.Format", "<gray>Player</gray> <dark_gray>-</dark_gray> <gray>{player}</gray>");
-        createConfigKey("Ranks.Player.Tab.Priority", 0);
+        createConfigKey("Ranks.Player.Tab.Priority", 1);
         createConfigKey("Ranks.Player.Chat.Active", true);
         createConfigKey("Ranks.Player.Chat.Format", "<gray>Player</gray> <dark_gray>-</dark_gray> <gray>{player}</gray><dark_gray>:</dark_gray> <white>{message}</white>");
         createConfigKey("Ranks.Player.NameTag.Active", true);
