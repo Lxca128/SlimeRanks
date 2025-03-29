eced764579a2220d2c130cc167c85c7a7c099038
Lxca <luca@lxca.de>
2025-03-29 11:23:26 +0100

    Remove unused import for Collections in RankManager.java

--- src/main/java/de/lxca/slimeRanks/objects/RankManager.java
+++ src/main/java/de/lxca/slimeRanks/objects/RankManager.java
@@ -11,7 +11,6 @@ import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 
