95bf946f4e0d563b956f27e518bd31677d2442a1
Lxca <luca@lxca.de>
2025-04-05 16:19:20 +0200

    Adjust the name tag display logic on join by using the recent created method

--- src/main/java/de/lxca/slimeRanks/listeners/PlayerJoinListener.java
+++ src/main/java/de/lxca/slimeRanks/listeners/PlayerJoinListener.java
@@ -3,7 +3,6 @@ package de.lxca.slimeRanks.listeners;
 import de.lxca.slimeRanks.objects.Rank;
 import de.lxca.slimeRanks.objects.RankManager;
 import de.lxca.slimeRanks.objects.UpdateChecker;
-import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,7 +13,8 @@ public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        Rank rank = RankManager.getInstance().getPlayerRank(player);
+        RankManager rankManager = RankManager.getInstance();
+        Rank rank = rankManager.getPlayerRank(player);
 
         if (rank == null) {
             return;
@@ -25,8 +25,8 @@ public class PlayerJoinListener implements Listener {
             player.setPlayerListOrder(rank.getTabPriority());
         }
 
-        if (rank.nameTagIsActive() && player.getGameMode() != GameMode.SPECTATOR) {
-            RankManager.getInstance().addPlayerNameTag(player);
+        if (rankManager.shouldDisplayPlayerNameTag(player, true)) {
+            rankManager.addPlayerNameTag(player);
         }
 
         if (player.hasPermission("slimeranks.admin")) {
