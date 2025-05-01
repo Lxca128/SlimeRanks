d22a8d91c34ff2db38b400fff817f6c0cf9f9e64
Lxca <luca@lxca.de>
2025-05-01 10:50:43 +0200

    Lowercase the default rank names

--- src/main/java/de/lxca/slimeRanks/objects/configurations/RanksYml.java
+++ src/main/java/de/lxca/slimeRanks/objects/configurations/RanksYml.java
@@ -15,27 +15,27 @@ public class RanksYml extends Yml {
     @Override
     protected void setDefaultYmlKeys() {
         createConfigKey("ConfigVersion", 1);
-        createConfigKey("Ranks.Admin.Tab.Active", true);
-        createConfigKey("Ranks.Admin.Tab.Format", "<color:#e63946>Admin</color> <dark_gray>|</dark_gray> <gray>{player}</gray>");
-        createConfigKey("Ranks.Admin.Tab.Priority", 2);
-        createConfigKey("Ranks.Admin.Chat.Active", true);
-        createConfigKey("Ranks.Admin.Chat.Format", "<color:#e63946>Admin</color> <dark_gray>|</dark_gray> <gray>{player}</gray> <dark_gray>»</dark_gray> <color:#ededed>{message}</color>");
-        createConfigKey("Ranks.Admin.Chat.ColoredMessages", true);
-        createConfigKey("Ranks.Admin.NameTag.Active", true);
-        createConfigKey("Ranks.Admin.NameTag.Format", "<color:#e63946>Admin</color> <dark_gray>|</dark_gray> <gray>{player}</gray>");
-        createConfigKey("Ranks.Admin.NameTag.HideOnSneak", false);
-        createConfigKey("Ranks.Admin.RankPriority", 1);
-        createConfigKey("Ranks.Admin.Permission", "slimeranks.rank.admin");
-        createConfigKey("Ranks.Player.Tab.Active", true);
-        createConfigKey("Ranks.Player.Tab.Format", "<color:#b0b0b0>Player</color> <dark_gray>|</dark_gray> <gray>{player}</gray>");
-        createConfigKey("Ranks.Player.Tab.Priority", 1);
-        createConfigKey("Ranks.Player.Chat.Active", true);
-        createConfigKey("Ranks.Player.Chat.Format", "<color:#b0b0b0>Player</color> <dark_gray>|</dark_gray> <gray>{player}</gray> <dark_gray>»</dark_gray> <color:#ededed>{message}</color>");
-        createConfigKey("Ranks.Player.Chat.ColoredMessages", false);
-        createConfigKey("Ranks.Player.NameTag.Active", true);
-        createConfigKey("Ranks.Player.NameTag.Format", "<color:#b0b0b0>Player</color> <dark_gray>|</dark_gray> <gray>{player}</gray>");
-        createConfigKey("Ranks.Player.NameTag.HideOnSneak", false);
-        createConfigKey("Ranks.Player.RankPriority", 0);
+        createConfigKey("Ranks.admin.Tab.Active", true);
+        createConfigKey("Ranks.admin.Tab.Format", "<color:#e63946>Admin</color> <dark_gray>|</dark_gray> <gray>{player}</gray>");
+        createConfigKey("Ranks.admin.Tab.Priority", 2);
+        createConfigKey("Ranks.admin.Chat.Active", true);
+        createConfigKey("Ranks.admin.Chat.Format", "<color:#e63946>Admin</color> <dark_gray>|</dark_gray> <gray>{player}</gray> <dark_gray>»</dark_gray> <color:#ededed>{message}</color>");
+        createConfigKey("Ranks.admin.Chat.ColoredMessages", true);
+        createConfigKey("Ranks.admin.NameTag.Active", true);
+        createConfigKey("Ranks.admin.NameTag.Format", "<color:#e63946>Admin</color> <dark_gray>|</dark_gray> <gray>{player}</gray>");
+        createConfigKey("Ranks.admin.NameTag.HideOnSneak", false);
+        createConfigKey("Ranks.admin.RankPriority", 1);
+        createConfigKey("Ranks.admin.Permission", "slimeranks.rank.admin");
+        createConfigKey("Ranks.default.Tab.Active", true);
+        createConfigKey("Ranks.default.Tab.Format", "<color:#b0b0b0>Player</color> <dark_gray>|</dark_gray> <gray>{player}</gray>");
+        createConfigKey("Ranks.default.Tab.Priority", 1);
+        createConfigKey("Ranks.default.Chat.Active", true);
+        createConfigKey("Ranks.default.Chat.Format", "<color:#b0b0b0>Player</color> <dark_gray>|</dark_gray> <gray>{player}</gray> <dark_gray>»</dark_gray> <color:#ededed>{message}</color>");
+        createConfigKey("Ranks.default.Chat.ColoredMessages", false);
+        createConfigKey("Ranks.default.NameTag.Active", true);
+        createConfigKey("Ranks.default.NameTag.Format", "<color:#b0b0b0>Player</color> <dark_gray>|</dark_gray> <gray>{player}</gray>");
+        createConfigKey("Ranks.default.NameTag.HideOnSneak", false);
+        createConfigKey("Ranks.default.RankPriority", 0);
         saveYmlConfig();
     }
 }
