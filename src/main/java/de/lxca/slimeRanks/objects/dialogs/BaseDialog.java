3228cdc5c94889793447772a88a8bf05344494b2
Luca <luca@lxca.de>
2025-09-28 12:49:37 +0200

    Add BaseDialog abstract class for dialog management

---
+++ src/main/java/de/lxca/slimeRanks/objects/dialogs/BaseDialog.java
@@ -0,0 +1,29 @@
+package de.lxca.slimeRanks.objects.dialogs;
+
+import de.lxca.slimeRanks.objects.Rank;
+import io.papermc.paper.dialog.Dialog;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class BaseDialog {
+
+    protected final Player player;
+    protected final Rank rank;
+    protected Dialog dialog;
+
+    public BaseDialog(@NotNull Player player, @NotNull Rank rank, boolean buildDialog) {
+        this.player = player;
+        this.rank = rank;
+        if (buildDialog) {
+            this.dialog = buildDialog();
+        }
+    }
+
+    protected abstract @NotNull Dialog buildDialog();
+
+    public void open() {
+        player.showDialog(dialog);
+        player.playSound(player, Sound.ITEM_BOOK_PAGE_TURN, 1, 1);
+    }
+}
